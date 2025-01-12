bc66a43c384eb0869a5a9a8755e8dd36a90ea2b7
William <wilenrike@gmail.com>
2025-01-11 21:53:43 -0600

    update: Refactor Table promises

--- src/core/table.ts
+++ src/core/table.ts
@@ -12,7 +12,7 @@ export class Table<T extends {}> implements DatabaseTable<T> {
   private _name: string;
   private _recordsMap: Map<string, T>;
   private _recordsArray: T[];
-  private _PkDefinition: (keyof T)[];
+  private _pkDefinition: (keyof T)[];
 
   /**
    * @param name Name of the table
@@ -22,7 +22,7 @@ export class Table<T extends {}> implements DatabaseTable<T> {
     this._name = name;
     this._recordsMap = new Map();
     this._recordsArray = [];
-    this._PkDefinition = this.validatePKDefinition(pkDefinition);
+    this._pkDefinition = this.validatePKDefinition(pkDefinition);
   }
 
   get name(): string { return this._name; }
@@ -37,17 +37,17 @@ export class Table<T extends {}> implements DatabaseTable<T> {
   }
 
   private hasNotPkDefinition(): boolean {
-    return this._PkDefinition.length === 0;
+    return this._pkDefinition.length === 0;
   }
 
   private isSingleKey(): boolean {
-    return this._PkDefinition.length === 1;
+    return this._pkDefinition.length === 1;
   }
 
   private isPartialRecordPartOfPk(record: Partial<T>): boolean {
     if (this.hasNotPkDefinition()) return false;
-    if (this.isSingleKey()) return record[this._PkDefinition[0]] !== undefined;
-    return this._PkDefinition.some(field => record[field] !== undefined);
+    if (this.isSingleKey()) return record[this._pkDefinition[0]] !== undefined;
+    return this._pkDefinition.some(field => record[field] !== undefined);
   }
 
   /**
@@ -68,8 +68,8 @@ export class Table<T extends {}> implements DatabaseTable<T> {
     }
 
     return this.isSingleKey() 
-      ? String(getPkValue(this._PkDefinition[0])) 
-      : this._PkDefinition.map(getPkValue).join('-');
+      ? String(getPkValue(this._pkDefinition[0])) 
+      : this._pkDefinition.map(getPkValue).join('-');
   }
 
   /**
@@ -80,7 +80,7 @@ export class Table<T extends {}> implements DatabaseTable<T> {
    */
   private generatePkForUpdate(updatedFields: Partial<T>, registeredRecord: T): { newPk: string; oldPk: string } {
     if (this.isSingleKey()) {
-      const pkDefinitionName = this._PkDefinition[0];
+      const pkDefinitionName = this._pkDefinition[0];
       return { 
         newPk: String(updatedFields[pkDefinitionName] ?? registeredRecord[pkDefinitionName]),
         oldPk: String(registeredRecord[pkDefinitionName])
@@ -88,8 +88,8 @@ export class Table<T extends {}> implements DatabaseTable<T> {
     }
     // Composite key
     return {
-      newPk: this._PkDefinition.map(pkName => updatedFields[pkName] ?? registeredRecord[pkName]).join('-'),
-      oldPk: this._PkDefinition.map(pkName => registeredRecord[pkName]).join('-')
+      newPk: this._pkDefinition.map(pkName => updatedFields[pkName] ?? registeredRecord[pkName]).join('-'),
+      oldPk: this._pkDefinition.map(pkName => registeredRecord[pkName]).join('-')
     }
   }
 
@@ -103,7 +103,7 @@ export class Table<T extends {}> implements DatabaseTable<T> {
   private checkIfPkIsInUse(primaryKey: string): void {
     if (this._recordsMap.has(primaryKey)) {
       throw new DuplicatePrimaryKeyValueError(
-        this._PkDefinition.join(', '),
+        this._pkDefinition.join(', '),
         primaryKey
       );
     }
@@ -120,34 +120,27 @@ export class Table<T extends {}> implements DatabaseTable<T> {
 
   public size(): number { return this._recordsArray.length; }
 
-  public insert(record: T): Promise<T> {
-    return new Promise((resolve) => {
+  public async insert(record: T): Promise<T> {
+    const newRecord = { ...record };
+    this.insertInMap(newRecord);
+    this._recordsArray.push(newRecord);
+    return { ...record };
+  }
+
+  public async bulkInsert(records: T[]): Promise<void> {
+    for (let record of records) {
       const newRecord = { ...record };
       this.insertInMap(newRecord);
       this._recordsArray.push(newRecord);
-      resolve({ ...record });
-    });
+    }
   }
 
-  public bulkInsert(records: T[]): Promise<void> {
-    return new Promise((resolve) => {
-      for (let record of records) {
-        const newRecord = { ...record };
-        this.insertInMap(newRecord);
-        this._recordsArray.push(newRecord);
-      }
-      resolve();
-    });
-  }
+  public async findByPk(primaryKey: Partial<T>): Promise<T | null> {
+    // throw exception if primary key is not defined
+    const generatedPk = this.generatePK(primaryKey);
+    const finded = this._recordsMap.get(generatedPk);
 
-  public findByPk(primaryKey: Partial<T>): Promise<T | null> {
-    return new Promise((resolve) => {
-      // throw exception if primary key is not defined
-      const generatedPk = this.generatePK(primaryKey);
-      const finded = this._recordsMap.get(generatedPk);
-
-      resolve(finded ? { ...finded } : null);
-    });
+    return finded ? { ...finded } : null;
   }
 
   private selectFields(fields: (keyof T)[], record: T): Partial<T> {
@@ -158,23 +151,21 @@ export class Table<T extends {}> implements DatabaseTable<T> {
     return newRecord;
   }
 
-  public select(fields: (keyof T)[], where: Filter<T>): Promise<Partial<T>[]> {
-    return new Promise((resolve) => {
-      const compiledFilter = compileFilter(where);
-      const result: Partial<T>[] = [];
+  public async select(fields: (keyof T)[], where: Filter<T>): Promise<Partial<T>[]> {
+    const compiledFilter = compileFilter(where);
+    const result: Partial<T>[] = [];
 
-      for (let currentRecord of this._recordsArray) {
+    for (let currentRecord of this._recordsArray) {
 
-        if (!matchRecord(currentRecord, compiledFilter)) continue;
+      if (!matchRecord(currentRecord, compiledFilter)) continue;
 
-        if (fields.length === 0) {
-          result.push({ ...currentRecord });
-          continue;
-        }
-        result.push(this.selectFields(fields, currentRecord));
+      if (fields.length === 0) {
+        result.push({ ...currentRecord });
+        continue;
       }
-      resolve(result);
-    });
+      result.push(this.selectFields(fields, currentRecord));
+    }
+    return result;
   }
 
   public async update(updatedFields: Partial<T>, where: Filter<T>): Promise<number> {
