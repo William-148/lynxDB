a9ba739cc71b1dc4bdf817716d6548e0780c8470
William <wilenrike@gmail.com>
2025-01-26 21:24:58 -0600

    add: Config tests

--- src/core/config.ts
+++ src/core/config.ts
@@ -14,6 +14,13 @@ export class Config {
     }
   }
 
+  /**
+   * Creates a new instance of Config merging the existing configuration with the new options.
+   * 
+   * @param existingConfig Existing configuration to be updated
+   * @param newOptions New options to be added to the existing configuration
+   * @returns New object with the updated configuration
+   */
   static fromOptions(existingConfig: Config, newOptions: ConfigOptions): Config {
     return new Config({
       ...existingConfig.options,
@@ -21,8 +28,21 @@ export class Config {
     });
   }
 
+  /**
+   * Retrieves a new object with the current configuration options.
+   * 
+   * @returns The current configuration options
+   */
+  public getOptions(): Required<ConfigOptions> {
+    return { ...this.options };
+  }
+
   get<K extends keyof ConfigOptions>(key: K): Required<ConfigOptions>[K] {
     return this.options[key];
+  }
+
+  set<K extends keyof ConfigOptions>(key: K, value: ConfigOptions[K]): void {
+    this.options[key] = value as Required<ConfigOptions>[K];
   }
 }
 
